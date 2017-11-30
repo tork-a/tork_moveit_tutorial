bd3e51ccbbe705b5dc0ccc0c4d6610e077953deb
Yosuke Yamamoto <yamamoto.yosuke@gmail.com>
2017-11-30 19:05:33 +0900

    mod for motion with constraints and scene objects

--- script/nextage_moveit_tutorial_poses_object_constraint.py
+++ script/nextage_moveit_tutorial_poses_object_constraint.py
@@ -16,6 +16,18 @@ if __name__ == '__main__':
     
     group = MoveGroupCommander("right_arm")
     
+    
+    # Initialize the Planning Scene
+    rospy.loginfo( "Setting the Planning Scene...")
+    scene = PlanningSceneInterface()
+    rospy.sleep(2)
+
+    scene.remove_world_object()    # Remove all objects first
+    rospy.sleep(2)
+    
+    rospy.loginfo( "All objects Removed : {}".format( scene.get_known_object_names() ) )
+    
+    
     # Pose Target 1
     rospy.loginfo( "Start Pose Target 1")
     pose_target_1 = Pose()
@@ -33,21 +45,21 @@ if __name__ == '__main__':
     group.set_pose_target( pose_target_1 )
     group.go()
     
-    # Add Object to Planning Scene
-    rospy.loginfo( "Planning Scene Settings")
-    scene = PlanningSceneInterface()
-
-    rospy.sleep(2)   # Waiting for PlanningSceneInterface
-
+    
+    # Add Object to the Planning Scene
+    rospy.loginfo( "Add Objects to the Planning Scene..." )
     box_pose = PoseStamped()
     box_pose.header.frame_id = group.get_planning_frame()
-    box_pose.pose.position.x = 0.35
+    box_pose.pose.position.x = 0.3
     box_pose.pose.position.y = -0.3
-    box_pose.pose.position.z = -0.2
+    box_pose.pose.position.z = -0.25
     box_pose.pose.orientation.w = 1.0
-    scene.add_box( 'box_object', box_pose, ( 0.3, 0.1, 0.5 ) )
-
+    
+    scene.add_box( 'box_object', box_pose, ( 0.4, 0.1, 0.5 ) )
+    rospy.sleep(2)
+        
     rospy.loginfo( "Scene Objects : {}".format( scene.get_known_object_names() ) )
+    
     
     # Set Path Constraint
     constraints = Constraints()
@@ -57,15 +69,16 @@ if __name__ == '__main__':
     orientation_constraint.header.frame_id = group.get_planning_frame()
     orientation_constraint.link_name = group.get_end_effector_link()
     orientation_constraint.orientation = pose_target_1.orientation
-    orientation_constraint.absolute_x_axis_tolerance = 0.1
-    orientation_constraint.absolute_y_axis_tolerance = 0.1
-    orientation_constraint.absolute_z_axis_tolerance = 3.1415
+    orientation_constraint.absolute_x_axis_tolerance = 3.1415
+    orientation_constraint.absolute_y_axis_tolerance = 0.05
+    orientation_constraint.absolute_z_axis_tolerance = 0.05
     orientation_constraint.weight = 1.0
     
     constraints.orientation_constraints.append( orientation_constraint )
     
     group.set_path_constraints( constraints )
     rospy.loginfo( "Get Path Constraints:\n{}".format( group.get_path_constraints() ) )
+    
     
     # Pose Target 2
     rospy.loginfo( "Start Pose Target 2")
@@ -79,12 +92,22 @@ if __name__ == '__main__':
     pose_target_2.orientation.w = 0.707
     
     group.set_planner_id( "RRTConnectkConfigDefault" )
-    group.set_num_planning_attempts( 10 )
     group.allow_replanning( True )
-    group.allow_looking( True )
     
     rospy.loginfo( "Set Target to Pose:\n{}".format( pose_target_2 ) )
     
-    group.set_pose_target( pose_target_2 )
-    group.go()
+    xyz =  [ pose_target_2.position.x,
+             pose_target_2.position.y,
+             pose_target_2.position.z ]
+    group.set_position_target( xyz )
+    result_p = group.go()
+    
+    rospy.loginfo( "Moving to the Position Executed... {}".format( result_p ) )
+    
+    group.clear_path_constraints()
+    
+    if result_p:
+        group.set_pose_target( pose_target_2 )
+        result_o = group.go()
+        rospy.loginfo( "Adjusting the Orientation Executed... {}".format( result_o ) )
     
